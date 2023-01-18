ca7efc0eaddda4df42615d1646d4c12519d19f80
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-01-18 20:17:17 +0200

    done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Statistics from '../Statistics';
+import FeedbackOptions from '../FeedbackOptions';
+import Section from '../Section';
+import { Container } from './App.styled';
+
+
+export class App extends Component {
+  static defaultProps = {
+    // initialValue: 0,
+  };
+
+  state = {
+    good: 0,
+    neutral: 0, 
+    bad: 0,
+  };
+
+  handleIncrement = (vote) => {
+    this.setState(prevState => {
+      let stateOptions = {}
+      stateOptions[vote] = prevState[vote] + 1
+      return stateOptions
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }; 
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good / (this.state.good + this.state.neutral + this.state.bad) * 100).toFixed(2)
+  }; 
+
+  render() {
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleIncrement} />
+        </Section>
+        {(this.state.good > 0 || this.state.neutral > 0 || this.state.bad) > 0 && (
+            <Section title="Statistics">
+              <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} countTotal={this.countTotalFeedback()} countPositive={this.countPositiveFeedbackPercentage()} />
+            </Section>
+        )}
+      </Container>
+    );
+  }
+
+}
---
+++ src/components/Section/Section.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContainerSection, Title } from './Section.styled';
+
+const Section = ( {title, children} ) => {
+    return (
+        <ContainerSection>
+            <Title>{title}</Title>
+            {children}
+        </ContainerSection>
+    );
+}
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+
+export default Section;
